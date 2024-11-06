81312ed782e012b9f1f17ba62b3a6010b42166c7
ibxccc123 <peng.c.danny@gmail.com>
2024-11-06 15:34:14 -0500

    Changed how durability works across plates in helmets

--- src/mod.ts
+++ src/mod.ts
@@ -19,14 +19,17 @@ class Mod implements IPostDBLoadMod
 
         if (config.ModifyHelmets) 
         {
-            //IDs of all helmets that should be ignored.  The ID below is the DevTac Ronin ballistic helmet as it already covers the full head
-            const ignoredHelmetsList = ["5b4329f05acfc47a86086aa1"]
+            //IDs of all helmets that should be ignored.
+            const ignoredHelmetsList = []
 
             //The armored headwear to be modified.  Filters out headwear that does not have slots since clothing headwear has 0 slots
             const helmets = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.HEADWEAR) && x._props.Slots.length > 1 && !(ignoredHelmetsList.includes(x._id)));
 
             //The built-in plate inserts to be used for referencing the armor value and durability of a helmet
             const builtInInserts = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.BUILT_IN_INSERTS)) 
+
+            //List of all helmet plate inserts that will be set to 1 durability after helmets iteration is complete
+            let helmetPlates = [];
 
             for (const helmet of helmets) 
             {
@@ -43,16 +46,50 @@ class Mod implements IPostDBLoadMod
                     "NeckBack"
                 ];
 
-                //Finds the plate item in the top plate slot of the helmet.
-                const slotId = helmet._props.Slots.findIndex(slot => slot._name.toLowerCase() == "helmet_top");
-                const topPlateId = helmet._props.Slots[slotId]._props.filters[0].Plate;
-                const topPlate = builtInInserts.find(ins => ins._id == topPlateId);
+                let numOfPlates = 0;
+                let armorClass;
+                let plateDurability;
 
-                // Modifies the armor class and durability of the helmet by referencing the top plate's props.  Durability is multiplied by 2
-                helmet._props.armorClass = topPlate._props.armorClass;
-                helmet._props.Durability = topPlate._props.Durability * 2;
-                helmet._props.MaxDurability = topPlate._props.MaxDurability * 2;
+                //Iterates through all slots in the helmet to find the plate inserts, then adds the plate insert to helmetPlates[] for later durability setting
+                for (const slot of helmet._props.Slots) 
+                {
+                    let slotName = slot._name.toLowerCase(); 
+                    if (slotName == "helmet_top" ||
+                        slotName == "helmet_back" ||
+                        slotName == "helmet_eyes" ||
+                        slotName == "helmet_jaw" ||
+                        slotName == "helmet_ears")
+                    {
+                        const plateId = slot._props.filters[0].Plate;
+                        const plate = builtInInserts.find(ins => ins._id == plateId);
+                        //Stores the helmet's armor class and durability in armorClass and plateDurability
+                        if (armorClass == null || plateDurability == null) 
+                        {
+                            armorClass = plate._props.armorClass;
+                            plateDurability = plate._props.MaxDurability;
+                        }
+                        if (!helmetPlates.includes(plate)) 
+                        {
+                            helmetPlates.push(plate);
+                        }
+                        numOfPlates++;
+                    }
+                }
+
+                // Modifies the armor class and durability of the helmet
+                helmet._props.armorClass = armorClass;
+                helmet._props.Durability = (plateDurability*numOfPlates) - numOfPlates;
+                helmet._props.MaxDurability = (plateDurability*numOfPlates) - numOfPlates;
+                
             }
+
+            //Set the plate inserts in the helmet to 1 durability
+            for (const plate of helmetPlates) 
+            {
+                plate._props.Durability = 1;
+                plate._props.MaxDurability = 1;
+            }
+
         }
 
         if (config.ModifyFaceShields) 
@@ -68,7 +105,7 @@ class Mod implements IPostDBLoadMod
                 }
             }
 
-            //Increases armor class of face shields by 2, multiplies the durability by 2, and modifies face shields to cover the full face.
+            //Increases armor class of face shields by 2 and modifies face shields to cover the full face.
             const ceiling = 6; 
             for (const visor of visors) 
             {
@@ -78,8 +115,6 @@ class Mod implements IPostDBLoadMod
                     newArmorClass = ceiling;
                 }
                 visor._props.armorClass = newArmorClass.toString();
-                visor._props.Durability *= 2;
-                visor._props.MaxDurability *= 2;
                 visor._props.armorColliders = 
                 [
                     "HeadCommon",
